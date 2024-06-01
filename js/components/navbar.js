f295b71604b39a85cc0f7e3a70d3cea1e3a28fcb
Leaqwid <tambarobert2517@gmail.com>
2024-06-01 19:18:35 +0300

    Design Navbar + Fix navbar and contentContainer

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -1,10 +1,10 @@
-// navbar.js
+
 function loadNavbar() {
     var navbarHtml = `
     <nav>
         <a class="logoWebsite" href="/index.html" >Acasa</a>
         <div class="nav-links">
-            <ul>
+            <ul >
                 <li><a href="#" id="aboutLink">Despre mine</a></li>
                 <li><a href="#" id="projectsLink">Proiectele mele</a></li>
                 <li><a href="#" id="howLink">Cum am făcut acest website</a></li>
